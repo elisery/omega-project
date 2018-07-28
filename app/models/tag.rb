743451141a8aaa8fd5d83cf2259003f6c0d1cbc2
GautamAnkita <reply.ankita@gmail.com>
2018-07-28 10:20:50 -0700

    Create users tags and tagging migrations

---
+++ app/models/tag.rb
@@ -0,0 +1,20 @@
+class Tag < ApplicationRecord
+
+    belongs_to company, optional: true
+
+    validates :name, presence: true, uniqueness: true
+    before_validation :downcase_name
+  
+    has_many :taggings, dependent: :destroy
+    has_many :companies, through: :taggings
+
+    def humanized_name
+        name&.humanize
+    end
+
+
+    private
+    def downcase_name
+      self.name&.downcase!
+    end
+end
