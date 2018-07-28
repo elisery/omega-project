3a011259d66b5a730c86fb9ae3c4059cd6ffc53e
Elise Yohm <elisery@users.noreply.github.com>
2018-07-28 10:33:41 -0700

    Add basic routes (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :news, only: [:create, :destroy]
+    resources :events, only: [:create, :destroy]
+    resources :technologies, only: [:create, :destroy]
+    resources :organizations
+    resources :users, [:edit, :update, :destroy]
+  end
+
+  resources :directory do
+    resources :companies
+  end
+
+  resources :events 
+
+  resources :news
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
+
+  get('/about', { to: 'about#index' })
+
 end
