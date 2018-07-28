20f4ed4c1f03a4cfafe0c2f26eaa5130577db2f1
Juliana Martins <julianamm88@gmail.com>
2018-07-28 12:50:50 -0700

    start the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :news, only: [:create, :destroy]
-    resources :events, only: [:create, :destroy]
-    resources :technologies, only: [:create, :destroy]
-    resources :organizations
-    resources :users, [:edit, :update, :destroy]
-  end
+  # namespace :admin do
+  #   resources :news, only: [:create, :destroy]
+  #   resources :events, only: [:create, :destroy]
+  #   resources :technologies, only: [:create, :destroy]
+  #   resources :organizations
+  #   resources :users, [:edit, :update, :destroy]
+  # end
 
   resources :directory do
     resources :companies
